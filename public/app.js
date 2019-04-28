2a46b9018c584e34bfd575933a03d0b787186323
gr3164 <pin.pinigin@mail.ru>
2019-04-28 09:29:31 +0500

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -17,4 +17,24 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+	//Вывод кол-во item'ов в корзине
+	alert('Items in your cart: ' + cart_get_number_of_items());
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //Получаем ключ
+		var value = window.localStorage.getItem(key);//Получаем значение
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
 }
