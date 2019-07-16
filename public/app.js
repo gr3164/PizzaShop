ba488873e4a18a1b45568eeb9d1b29fdf3588617
gr3164 <pin.pinigin@mail.ru>
2019-07-16 12:00:44 +0500

    Replace card contents on cancel

--- public/app.js
+++ public/app.js
@@ -73,7 +73,11 @@ function cart_get_orders()
 function cancel_order(id)
 {
 	window.localStorage.clear();
+
 	update_orders_input();
 	update_orders_button();
+
+	$('#cart').text('Your cart is now empty');
+
 	return false;
 }
